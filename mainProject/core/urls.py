f657e718246c2b2eef2896d6e1f1d1104e6fdb1e
idris <idris.sabanli@gmail.com>
2020-08-21 19:35:04 +0400

    url students

--- mainProject/core/urls.py
+++ mainProject/core/urls.py
@@ -4,7 +4,7 @@ from core.views import *
 app_name = 'core'
 
 urlpatterns = [
-    path("students", StudentListView.as_view(), name="student-list"),
+    path("", StudentListView.as_view(), name="student-list"),
     path("students/<int:pk>", StudentDetailView.as_view(), name="student-detail"),
     path("students-rating/<int:pk>", GiveRatingView.as_view(), name="student-rating"),
     path("students-filter/", StudentAjaxListView.as_view(), name="student-list-ajax"),
